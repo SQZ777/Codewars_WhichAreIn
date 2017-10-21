309439dd2f885e56456980565e61fd423ed090be
sqz777 <sqz777isme@gmail.com>
2017-10-21 14:09:59 +0800

    refactor 多餘的Method

--- Codewars_WhichAreIn/UnitTest1.cs
+++ Codewars_WhichAreIn/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Codewars_WhichAreIn
 {
@@ -25,26 +26,26 @@ namespace Codewars_WhichAreIn
         [TestMethod]
         public void Input_ss_arpAndarp_Should_Be_arpOfArray()
         {
-            var actual = Kata.inArray(new string[] {"ss", "arp"}, new string[] {"arp"});
-            var expected = new string[] {"arp"};
-            CollectionAssert.AreEqual(expected,actual);
+            var actual = Kata.inArray(new string[] { "ss", "arp" }, new string[] { "arp" });
+            var expected = new string[] { "arp" };
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Input_ss_arp_And_art_starpoint_Should_Be_arp()
         {
-            var actual = Kata.inArray(new string[] {"ss", "arp"}, new string[] {"art", "starpoint"});
-            var expected =new string[]{"arp"};
-            CollectionAssert.AreEqual(expected,actual);
+            var actual = Kata.inArray(new string[] { "ss", "arp" }, new string[] { "art", "starpoint" });
+            var expected = new string[] { "arp" };
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void CodewarsExampleTest()
         {
-            var actual = Kata.inArray(new string[] {"arp", "live", "strong"},
-                new string[] {"lively", "alive", "harp", "sharp", "armstrong"});
+            var actual = Kata.inArray(new string[] { "arp", "live", "strong" },
+                new string[] { "lively", "alive", "harp", "sharp", "armstrong" });
             var expected = new string[] { "arp", "live", "strong" };
-            CollectionAssert.AreEqual(expected,actual);
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 
@@ -52,18 +53,12 @@ namespace Codewars_WhichAreIn
     {
         public static string[] inArray(string[] array1, string[] array2)
         {
-            List<string> result = new List<string>();
+            var result = new List<string>();
             foreach (var a1 in array1)
-            {
                 foreach (var a2 in array2)
-                {   
-                    if (a2.Contains(a1)&& !result.Contains(a1))
-                    {
+                    if (a2.Contains(a1) && !result.Contains(a1))
                         result.Add(a1);
-                    }
-                }
-            }
-            return result.ToArray();
+            return result.OrderBy(x => x).ToArray();
         }
     }
 }
