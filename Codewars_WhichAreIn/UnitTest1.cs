3c2dba594929f3a92b0537ac81a2613617930724
sqz777 <sqz777isme@gmail.com>
2017-10-21 11:47:22 +0800

    PASS 2nd test case

--- Codewars_WhichAreIn/UnitTest1.cs
+++ Codewars_WhichAreIn/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Codewars_WhichAreIn
 {
@@ -7,26 +9,34 @@ namespace Codewars_WhichAreIn
         [TestMethod]
         public void Input_arpAnd_aaa_Should_Be_EmptyArray()
         {
-            var actual = Kata.inArray(new string[] {"arp"}, new string[] {"aaa"});
+            var actual = Kata.inArray(new string[] { "arp" }, new string[] { "aaa" });
             var expected = new string[] { };
-            CollectionAssert.AreEqual(expected,actual);
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Input_arpAndarp_Should_Be_arpOfArray()
         {
-            var actual = Kata.inArray(new string[] {"arp"}, new string[] {"arp"});
-            var expected = new string[] {"arp" };
-            CollectionAssert.AreEqual(expected,actual);
+            var actual = Kata.inArray(new string[] { "arp" }, new string[] { "arp" });
+            var expected = new string[] { "arp" };
+            CollectionAssert.AreEqual(expected, actual);
         }
-
+         
     }
 
     public class Kata
     {
         public static string[] inArray(string[] array1, string[] array2)
         {
-            return new string[]{};
+            List<string> result = new List<string>();
+            foreach (var s in array2)
+            { 
+                if (s.Equals(array1[0]) )
+                {
+                    result.Add(s);
+                }
+            }
+            return result.ToArray();
         }
     }
 }
